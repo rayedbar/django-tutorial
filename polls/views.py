1ff9636320c2a96cd42033104fae358d0906c9de
Rayed Bin Wahed <rayedbinwahed@gmail.com>
2018-12-02 21:00:48 +0600

    Update form action to post

--- polls/views.py
+++ polls/views.py
@@ -7,14 +7,18 @@ from . models import Question, Choice
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
+
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
+
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    return HttpResponse('You are looking at results of question %s.' % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -28,4 +32,5 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
+
         return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
